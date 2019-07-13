65cd5b3d47cf9b3e04b9f68716cbb9a707257e13
你huangzhongyy <1259275133@qq.com>
2019-07-13 13:04:23 +0800

    yes

--- weipinhui-HZweb/src/main/java/com/cssl/AppWeb.java
+++ weipinhui-HZweb/src/main/java/com/cssl/AppWeb.java
@@ -3,13 +3,17 @@ package com.cssl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 /**
  * @program: weipinghuiczx
  * @Date: 2019/7/10 10:40
  * @Author: Mr.Deng
  * @Description:
+ *
+ *
  */
 @SpringBootApplication
+//@EnableEurekaClient//eureka客户端
 public class AppWeb {
  public static void main(String[] args) {
   SpringApplication.run(AppWeb.class,args);
