d32db4fbb828928f57d9c27684e3985ca10ec675
你huangzhongyy <1259275133@qq.com>
2019-07-13 12:09:07 +0800

    加了个注册中心

--- weipinhui-eureka/src/main/java/com/weip/SpringCloud/SpringCloudEurekaApp.java
+++ weipinhui-eureka/src/main/java/com/weip/SpringCloud/SpringCloudEurekaApp.java
@@ -1,10 +1,20 @@
 package com.weip.SpringCloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * @program: weipinghuiczx
  * @Date: 2019/7/12 19:26
  * @Author: Mr.Deng
  * @Description:
  */
+@SpringBootApplication
+@EnableEurekaServer //EurekaServer服务器端启动类
 public class SpringCloudEurekaApp {
+ public static void main(String[] args) {
+  SpringApplication.run(SpringCloudEurekaApp.class,args);
+ }
+
 }
