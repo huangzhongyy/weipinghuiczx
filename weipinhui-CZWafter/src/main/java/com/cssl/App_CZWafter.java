a67376174a1c003de2bde83b528ead80b8ccef97
czw3 <www.1349827787@qq.com>
2019-07-15 17:40:01 +0800

    曹志威--加入俩个模块

---
+++ weipinhui-CZWafter/src/main/java/com/cssl/App_CZWafter.java
@@ -0,0 +1,17 @@
+package com.cssl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: weipinghuiczx
+ * @Date: 2019/7/15 17:32
+ * @Author: Mr.Deng
+ * @Description:
+ */
+@SpringBootApplication
+public class App_CZWafter {
+ public static void main(String[] args) {
+  SpringApplication.run(App_CZWafter.class,args);
+ }
+}
---
+++ weipinhui-CZWweb/src/main/java/com/cssl/App_CZWweb.java
@@ -0,0 +1,17 @@
+package com.cssl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: weipinghuiczx
+ * @Date: 2019/7/15 17:29
+ * @Author: Mr.Deng
+ * @Description:
+ */
+@SpringBootApplication
+public class App_CZWweb {
+ public static void main(String[] args) {
+  SpringApplication.run(App_CZWweb.class,args);
+ }
+}
