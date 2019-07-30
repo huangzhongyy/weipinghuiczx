d099b7fed829b6ed0173579a76e5610057307a94
huangzhongyy <1259275133@qq.com>
2019-07-30 20:18:05 +0800

    黄忠实体类提交

---
+++ weipinghui-common/src/main/java/com/weip/pojo/Goods_details.java
@@ -0,0 +1,25 @@
+package com.weip.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+/**
+ * @program: weipinghuiczx
+ * @Date: 2019/7/30 16:37
+ * @Author: Mr.Deng
+ * @Description:
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain=true)
+public class Goods_details {
+ private int flat_card_id;//id
+ private String the_fuselage_memory;
+ private String the_screen_size;
+ private int goods_id;//外键对应手机的ID
+}
